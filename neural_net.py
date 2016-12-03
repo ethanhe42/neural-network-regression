bdf894e0aface6d7d47e9823ad60767140f61eb1
yihui-he <yihuihe@foxmail.com>
2016-12-03 09:37:22 +0800

    templete

--- neural_net.py
+++ neural_net.py
@@ -45,33 +45,13 @@ class TwoLayerNet(object):
   """
   A two-layer fully-connected neural network. The net has an input dimension of
   N, a hidden layer dimension of H, and performs classification over C classes.
-  We train the network with a softmax loss function and L2 regularization on the
-  weight matrices. The network uses a ReLU nonlinearity after the first fully
-  connected layer.
-
-  In other words, the network has the following architecture:
-
-  input - fully connected layer - ReLU - fully connected layer - softmax
-
   The outputs of the second fully-connected layer are the scores for each class.
   """
 
   def __init__(self, input_size, hidden_size, output_size, std=1e-4,
     init_method="Normal", activation='relu'):
     """
-    Initialize the model. Weights are initialized to small random values and
-    biases are initialized to zero. Weights and biases are stored in the
-    variable self.params, which is a dictionary with the following keys:
-
-    W1: First layer weights; has shape (D, H)
-    b1: First layer biases; has shape (H,)
-    W2: Second layer weights; has shape (H, C)
-    b2: Second layer biases; has shape (C,)
-
-    Inputs:
-    - input_size: The dimension D of the input data.
-    - hidden_size: The number of neurons H in the hidden layer.
-    - output_size: The number of classes C.
+    Initialize the model.
     """
     self.params = {}
     self.params['W1'] = std * np.random.randn(input_size, hidden_size)
@@ -106,24 +86,6 @@ class TwoLayerNet(object):
     """
     Compute the loss and gradients for a two layer fully connected neural
     network.
-
-    Inputs:
-    - X: Input data of shape (N, D). Each X[i] is a training sample.
-    - y: Vector of training labels. y[i] is the label for X[i], and each y[i] is
-      an integer in the range 0 <= y[i] < C. This parameter is optional; if it
-      is not passed then we only return scores, and if it is passed then we
-      instead return the loss and gradients.
-    - reg: Regularization strength.
-
-    Returns:
-    If y is None, return a matrix scores of shape (N, C) where scores[i, c] is
-    the score for class c on input X[i].
-
-    If y is not None, instead return a tuple of:
-    - loss: Loss (data loss and regularization loss) for this batch of training
-      samples.
-    - grads: Dictionary mapping parameter names to gradients of those parameters
-      with respect to the loss function; has the same keys as self.params.
     """
     # Unpack variables from the params dictionary
     W1, b1 = self.params['W1'], self.params['b1']
@@ -132,37 +94,17 @@ class TwoLayerNet(object):
 
     # Compute the forward pass
     scores = None
-    #############################################################################
-    # TODO: Perform the forward pass, computing the class scores for the input. #
-    # Store the result in the scores variable, which should be an array of      #
-    # shape (N, C).                                                             #
-    #############################################################################
-
 
     inp=X.dot(W1)+b1
     a2 = self.acfunc(inp)
 
-	  # for convinient this is drop connect , drop rate= dropout-1
-
     scores=a2.dot(W2)+b2 # z3
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
 
-    # If the targets are not given then jump out, we're done
     if y is None:
       return scores
 
     # Compute the loss
     loss = None
-    #############################################################################
-    # TODO: Finish the forward pass, and compute the loss. This should include  #
-    # both the data loss and L2 regularization for W1 and W2. Store the result  #
-    # in the variable loss, which should be a scalar. Use the Softmax           #
-    # classifier loss. So that your results match ours, multiply the            #
-    # regularization loss by 0.5                                                #
-    #############################################################################
-    #do a softmax first
 
     if self.finalactivation:
         a3 = tanh(scores)
@@ -179,17 +121,9 @@ class TwoLayerNet(object):
 
     loss=lossf(a3 - y)+\
       0.5*reg*(np.sum(np.power(W1,2))+np.sum(np.power(W2,2)))
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
 
     # Backward pass: compute gradients
     grads = {}
-    #############################################################################
-    # TODO: Compute the backward pass, computing the derivatives of the weights #
-    # and biases. Store the results in the grads dictionary. For example,       #
-    # grads['W1'] should store the gradient on W1, and be a matrix of same size #
-    #############################################################################
 
     delta_3=dlossf(a3 - y)
 
@@ -204,9 +138,6 @@ class TwoLayerNet(object):
     delta_2=delta_3.dot(W2.T)*dF
     grads['W1']=X.T.dot(delta_2)+reg*W1
     grads['b1']=np.sum(delta_2,0)
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
 
     return loss, grads
 
@@ -219,20 +150,6 @@ class TwoLayerNet(object):
             dropout=0):
     """
     Train this neural network using stochastic gradient descent.
-
-    Inputs:
-    - X: A numpy array of shape (N, D) giving training data.
-    - y: A numpy array f shape (N,) giving training labels; y[i] = c means that
-      X[i] has label c, where 0 <= c < C.
-    - X_val: A numpy array of shape (N_val, D) giving validation data.
-    - y_val: A numpy array of shape (N_val,) giving validation labels.
-    - learning_rate: Scalar giving learning rate for optimization.
-    - learning_rate_decay: Scalar giving factor used to decay the learning rate
-      after each epoch.
-    - reg: Scalar giving regularization strength.
-    - num_iters: Number of steps to take when optimizing.
-    - batch_size: Number of training examples to use per step.
-    - verbose: boolean; if true print progress during optimization.
     """
     num_train = X.shape[0]
     iterations_per_epoch = max(num_train / batch_size, 1)
@@ -257,10 +174,6 @@ class TwoLayerNet(object):
       X_batch = None
       y_batch = None
 
-      #########################################################################
-      # TODO: Create a random minibatch of training data and labels, storing  #
-      # them in X_batch and y_batch respectively.                             #
-      #########################################################################
       if num_train >= batch_size:
         rand_idx=np.random.choice(num_train,batch_size)
       else:
@@ -271,19 +184,10 @@ class TwoLayerNet(object):
       if dropout>1:
         for param in ['W2','b2']:
        	  dropMask[param]=np.random.randn(*self.params[param].shape)<(dropout-1)
-      #########################################################################
-      #                             END OF YOUR CODE                          #
-      #########################################################################
 
       # Compute loss and gradients using the current minibatch
       loss, grads = self.loss(X_batch, y=y_batch, reg=reg, dropout=dropout,dropMask=dropMask)
 
-      #########################################################################
-      # TODO: Use the gradients in the grads dictionary to update the         #
-      # parameters of the network (stored in the dictionary self.params)      #
-      # using stochastic gradient descent. You'll need to use the gradients   #
-      # stored in the grads dictionary defined above.                         #
-      #########################################################################
       if np.isnan(grads['W1']).any() or np.isnan(grads['W2']).any() or \
         np.isnan(grads['b1']).any() or np.isnan(grads['b2']).any():
         continue
@@ -332,9 +236,6 @@ class TwoLayerNet(object):
         self.params[param]-=dx
       #Bug: learning rate should not decay at first epoch
       it+=1
-      #########################################################################
-      #                             END OF YOUR CODE                          #
-      #########################################################################
 
       selectbest=0
       if not selectbest:
@@ -377,47 +278,20 @@ class TwoLayerNet(object):
 
   def predict(self, X):
     """
-    Use the trained weights of this two-layer network to predict labels for
-    data points. For each data point we predict scores for each of the C
-    classes, and assign each data point to the class with the highest score.
-
-    Inputs:
-    - X: A numpy array of shape (N, D) giving N D-dimensional data points to
-      classify.
-
-    Returns:
-    - y_pred: A numpy array of shape (N,) giving predicted labels for each of
-      the elements of X. For all i, y_pred[i] = c means that X[i] is predicted
-      to have class c, where 0 <= c < C.
+    Use the trained weights of this two-layer network to predict
     """
     y_pred = None
-
-    ###########################################################################
-    # TODO: Implement this function; it should be VERY simple!                #
-    ###########################################################################
 
     #y_pred=relu(X.dot(self.params['W1'])+self.params['b1']).dot(self.params['W2'])+self.params['b2']
     y_pred=self.acfunc(X.dot(self.params['W1'])+self.params['b1']).dot(self.params['W2'])+self.params['b2']
     if self.finalactivation:
         y_pred = tanh(y_pred)
-    ###########################################################################
-    #                              END OF YOUR CODE                           #
-    ###########################################################################
 
     return y_pred
 
   def accuracy(self,X,y):
     """
-    Use the trained model to predict labels for X, and compute the accuracy.
-
-    Inputs:
-    - X: A numpy array of shape (N, D) giving N D-dimensional data points to
-      classify.
-    - y: A numpy array of shape (N,) giving the correct labels.
-
-    Returns:
-    - acc: Accuracy
-
+    compute the accuracy.
     """
     acc = (self.predict(X) == y).mean()
 
